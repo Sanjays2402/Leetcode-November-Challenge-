45897937fe84d5f8e9b1a23a02dc54df00848098
Sanjay <sanjays2402@gmail.com>
2020-11-18 15:18:06 +0530

    Create November 18 Solution.cpp

---
+++ November 18/November 18 Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+        
+        vector<int> temp = intervals[0];
+        for(int i=1; i<intervals.size(); i++)
+        {
+            if(intervals[i][0] <= temp[1])
+                temp[1] = max(temp[1], intervals[i][1]);
+            else
+            {
+                res.push_back(temp);
+                temp=intervals[i];
+            }
+        }
+        res.push_back(temp);
+        return res;
+    }
+};
